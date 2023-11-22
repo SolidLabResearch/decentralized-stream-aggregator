1c4ba59388c36c5d3cca5650358a8a9067ac6851
Kushagra Singh Bisen <sendtokush@icloud.com>
2023-11-22 15:26:09 +0100

    Update LDES location and changed pod cred

--- readFromPod.ts
+++ readFromPod.ts
@@ -1,5 +1,5 @@
 import { LDESinLDP, LDPCommunication, extractLdesMetadata } from "@treecg/versionawareldesinldp";
-let ldes_location = 'http://n061-14a.wall2.ilabt.iminds.be:3000/participant6/bvp-2min/';
+let ldes_location = 'http://n061-14a.wall2.ilabt.iminds.be:3000/participant6/bvp-1s/';
 import { readMembersRateLimited } from "./src/utils/ldes-in-ldp/EventSource"
 let ldes_in_ldp = new LDESinLDP(ldes_location, new LDPCommunication());
 let counter = 0;
