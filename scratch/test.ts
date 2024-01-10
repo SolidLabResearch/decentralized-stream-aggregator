386e0c22220c005066a8f49b2bcbd5661e6baf76
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-01-10 10:20:02 +0100

    final commit before webhook switch

---
+++ scratch/test.ts
@@ -0,0 +1,18 @@
+import { LDESinLDP, LDPCommunication } from "@treecg/versionawareldesinldp";
+// const pod_location_bvp = "http://n061-14a.wall2.ilabt.iminds.be:3000/participant6/bvp/";
+const pod_location_bvp = "http://localhost:3000/dataset_participant1/xyz/";
+
+async function main() {
+    let counter = 0;
+    const ldes = new LDESinLDP(pod_location_bvp, new LDPCommunication());
+    const stream = await ldes.readAllMembers();
+    stream.on("data", (data) => {
+        counter++;
+    });
+
+    stream.on("end", () => {
+        console.log(counter);
+    });
+}
+
+main();
