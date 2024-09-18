89bbbeecae19b2bfd208068c8b4892fbe7b9f586
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-09-18 16:10:23 +0200

    added logging in the HTTPServer.ts

--- src/server/HTTPServer.ts
+++ src/server/HTTPServer.ts
@@ -84,6 +84,7 @@ export class HTTPServer {
                             }
                         });
                         const latest_event = await latest_event_response.text();
+                        console.log(`The latest event is ${latest_event}`);
                         this.event_emitter.emit(`${ldes_stream_where_event_is_added}`, latest_event);
                         this.logger.info({}, 'webhook_notification_processed_and_emitted');
                     }
