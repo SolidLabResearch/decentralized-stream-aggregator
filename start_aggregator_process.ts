5bef6ece0d7f9bb8a83638003cdfc16bee6505c3
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-09-23 11:22:39 +0200

    Remove unused logger enums and added a script to initialize as a child process

---
+++ start_aggregator_process.ts
@@ -0,0 +1,38 @@
+const { spawn } = require('child_process');
+import * as fs from 'fs';
+const child = spawn('node', ['run', 'start', 'aggregation'], {
+    stdio: 'inherit',
+    shell: true
+});
+
+interface MemoryUsage {
+    rss: number;
+    heapTotal: number;
+    heapUsed: number;
+    external: number;
+}
+
+function logCPUMemoryUsage(logFile: string) {
+    const cpuUsage = process.cpuUsage();
+    const memoryUsage: MemoryUsage = process.memoryUsage();
+    const timestamp = Date.now();
+    const logData = `${timestamp},${cpuUsage.user},${cpuUsage.system},${memoryUsage.rss},${memoryUsage.heapTotal},${memoryUsage.heapUsed},${memoryUsage.external}\n`;
+    fs.appendFileSync(logFile, logData)
+}
+
+const logFile = 'stream-aggregator-resource-usage.csv';
+fs.writeFileSync(logFile, 'timestamp,cpu_user,cpu_system,rss,heapTotal,heapUsed,external\n');
+
+setInterval(() => {
+    logCPUMemoryUsage(logFile);
+}, 500);
+
+child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+});
+
+child.on('error', (err) => {
+    console.log(`Child process error: ${err}`);
+    process.exit(1);
+});
